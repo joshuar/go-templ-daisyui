3e44348e28d06dee7fe4505516c51b0df4439387
Joshua Rich <joshua.rich@gmail.com>
2025-01-02 11:14:47 +1000

    feat(modifiers/layout): :sparkles: layout is an inheritable modifier

--- layout.go
+++ layout.go
@@ -1,20 +1,55 @@
 // Copyright 2024 Joshua Rich <joshua.rich@gmail.com>.
 // SPDX-License-Identifier: MIT
 
-//go:generate go run golang.org/x/tools/cmd/stringer -type=Layout -linecomment -output layout_generated.go
 package components
 
-// Layout options. Vertical is the default in DaisyUI so it goes first.
+import (
+	"fmt"
+	"log/slog"
+)
+
+// Layout options.
 const (
-	VerticalLayout   Layout = iota // vertical
-	HorizontalLayout               // horizontal
+	VerticalLayout   Layout = iota + 1 // vertical
+	HorizontalLayout                   // horizontal
 )
 
 type Layout int
 
-// LayoutObject returns the given object class with an appropriate layout
-// modifier. For e.g., if Layout is VerticalLayout and object is menu, the return
-// value will be "menu-vertical".
-func (l Layout) LayoutObject(object string) string {
-	return object + "-" + l.String()
+// modifierLayout can be embedded into Components to allow setting a layout option
+// on the Component. The Component will need to handle rendering with the
+// appropriate size itself.
+type modifierLayout struct {
+	layout Layout
+}
+
+func (m *modifierLayout) SetLayout(layout Layout) {
+	m.layout = layout
+}
+
+// LayoutIsSet will return true if the Component has a layout.
+func (m *modifierLayout) LayoutIsSet() bool {
+	return m.layout > 0
+}
+
+type customisableLayout interface {
+	SetLayout(layout Layout)
+}
+
+// WithLayout adds the given layout to the Component.
+//
+//nolint:varnamelen // the var is copied into another with an appropriate name.
+func WithLayout[T any](layout Layout) Option[T] {
+	return func(c T) T {
+		component := &c
+
+		if settable, ok := any(component).(customisableLayout); ok {
+			settable.SetLayout(layout)
+		} else {
+			slog.Warn("WithLayout passed as option to component, but component does not support layout modifier.",
+				slog.String("component", fmt.Sprintf("%T", &c)))
+		}
+
+		return *component
+	}
 }
