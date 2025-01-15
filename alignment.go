31bbbc41c69a4d78b52e9efe19cb338a0a55e917
Joshua Rich <joshua.rich@gmail.com>
2025-01-15 11:52:03 +1000

    refactor(modifiers/alignment): :truck: renaming

--- alignment.go
+++ alignment.go
@@ -1,7 +1,6 @@
 // Copyright 2024 Joshua Rich <joshua.rich@gmail.com>.
 // SPDX-License-Identifier: MIT
 
-//go:generate go run golang.org/x/tools/cmd/stringer -type=Alignment -linecomment -output alignment_generated.go
 package components
 
 import (
@@ -30,7 +29,7 @@ func (m *modifierAlignment) SetAlignment(alignment Alignment) {
 	m.alignment = alignment
 }
 
-type customisableAlignment interface {
+type hasAlignmentModifier interface {
 	SetAlignment(alignment Alignment)
 }
 
@@ -39,7 +38,7 @@ func WithAlignment[T any](alignment Alignment) Option[T] {
 	return func(c T) T {
 		component := &c
 
-		if settable, ok := any(component).(customisableAlignment); ok {
+		if settable, ok := any(component).(hasAlignmentModifier); ok {
 			settable.SetAlignment(alignment)
 		} else {
 			slog.Warn("WithAlignment passed as option to component, but component does not support alignment modifier.",
