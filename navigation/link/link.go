622f7861e081a9e547069a719f381a53c029f47a
Joshua Rich <joshua.rich@gmail.com>
2025-02-14 15:10:42 +1000

    feat(components/link): :sparkles: add link component

---
+++ navigation/link/link.go
@@ -0,0 +1,76 @@
+// Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
+// SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+package link
+
+import (
+	"github.com/a-h/templ"
+	components "github.com/joshuar/go-templ-daisyui"
+	"github.com/joshuar/go-templ-daisyui/attributes"
+	"github.com/joshuar/go-templ-daisyui/modifiers/color"
+)
+
+type Option components.Option[*Props]
+
+type Props struct {
+	*attributes.Attributes
+	stateColor       color.StateColor
+	themeColor       color.ThemeColor
+	content          templ.Component
+	underlineOnHover bool
+}
+
+func WithThemeColor(themeColor color.ThemeColor) Option {
+	return func(p *Props) {
+		p.themeColor = themeColor
+	}
+}
+
+func WithStateColor(stateColor color.StateColor) Option {
+	return func(p *Props) {
+		p.stateColor = stateColor
+	}
+}
+
+func WithName(name attributes.Name) Option {
+	return func(p *Props) {
+		p.SetName(name)
+	}
+}
+
+func WithID(id attributes.ID) Option {
+	return func(p *Props) {
+		p.SetID(id)
+	}
+}
+
+func WithExtraAttributes(attrs templ.Attributes) Option {
+	return func(p *Props) {
+		p.AddAttributes(attrs)
+	}
+}
+
+// WithContent sets the content for the Button.
+func WithContent(content any) Option {
+	return func(p *Props) {
+		p.content = components.SetContent(content)
+	}
+}
+
+func WithUnderlineOnHover() Option {
+	return func(p *Props) {
+		p.underlineOnHover = true
+	}
+}
+
+func Build(options ...Option) *Props {
+	link := &Props{
+		Attributes: attributes.New(),
+	}
+
+	for _, option := range options {
+		option(link)
+	}
+
+	return link
+}
