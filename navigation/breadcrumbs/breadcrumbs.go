d91f165c9c2aeb402dbf97f6d0de681a9b01c8ae
Joshua Rich <joshua.rich@gmail.com>
2025-02-14 15:10:20 +1000

    feat(components/breadcrumbs): :sparkles: add breadcrumbs component

---
+++ navigation/breadcrumbs/breadcrumbs.go
@@ -0,0 +1,49 @@
+// Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
+// SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+package breadcrumbs
+
+import (
+	"github.com/a-h/templ"
+	components "github.com/joshuar/go-templ-daisyui"
+	"github.com/joshuar/go-templ-daisyui/attributes"
+	"github.com/joshuar/go-templ-daisyui/items"
+)
+
+type Option components.Option[*Props]
+
+type Props struct {
+	*attributes.Attributes
+	*items.Items
+}
+
+func WithID(id attributes.ID) Option {
+	return func(p *Props) {
+		p.SetID(id)
+	}
+}
+
+func WithExtraAttributes(attrs templ.Attributes) Option {
+	return func(p *Props) {
+		p.AddAttributes(attrs)
+	}
+}
+
+func WithCrumbs(crumbs ...templ.Component) Option {
+	return func(p *Props) {
+		p.ReplaceItems(crumbs...)
+	}
+}
+
+func Build(options ...Option) *Props {
+	link := &Props{
+		Attributes: attributes.New(),
+		Items:      items.New(),
+	}
+
+	for _, option := range options {
+		option(link)
+	}
+
+	return link
+}
