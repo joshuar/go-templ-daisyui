1b90e8ac3a69e6c480b0a146c699e4036a7dbb3f
Joshua Rich <joshua.rich@gmail.com>
2025-03-11 09:30:13 +1000

    feat(classes/width): :sparkles: add width utility classes

--- classes/classes.go
+++ classes/classes.go
@@ -1,24 +1,30 @@
 // Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
 // SPDX-License-Identifier: 	AGPL-3.0-or-later
 
+// Package classes can be inherited by components to provide the ability to set
+// custom classes on the component.
 package classes
 
 import "strings"
 
 type Class = string
 
+// Classes holds custom classes for a component.
 type Classes struct {
 	classes []Class
 }
 
+// HasClasses returns a boolean indicating whether any classes have been set.
 func (c *Classes) HasClasses() bool {
 	return len(c.classes) > 0
 }
 
+// ShowClasses returns a space-separated string with all classes.
 func (c *Classes) ShowClasses() string {
 	return strings.Join(c.classes, " ")
 }
 
+// AddClass will add the given class to the list of classes.
 func (c *Classes) AddClass(class Class) {
 	c.classes = append(c.classes, class)
 }
