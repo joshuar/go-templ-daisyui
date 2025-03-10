4bb5e5cd7e71aba493a632c4c5bc1160d940301b
Joshua Rich <joshua.rich@gmail.com>
2025-03-11 09:31:12 +1000

    feat(components/menu): :sparkles: support setting custom classes on menu component

--- navigation/menu/menu.go
+++ navigation/menu/menu.go
@@ -7,6 +7,7 @@ import (
 	"github.com/a-h/templ"
 	components "github.com/joshuar/go-templ-daisyui"
 	"github.com/joshuar/go-templ-daisyui/attributes"
+	"github.com/joshuar/go-templ-daisyui/classes"
 	"github.com/joshuar/go-templ-daisyui/items"
 	"github.com/joshuar/go-templ-daisyui/modifiers/breakpoints"
 	"github.com/joshuar/go-templ-daisyui/modifiers/color"
@@ -26,6 +27,7 @@ type (
 		title  string
 		layout Layout
 		*attributes.Attributes
+		*classes.Classes
 		size      size.ResponsiveSize
 		baseColor color.BaseColor
 		breakpoints.Breakpoints
@@ -98,10 +100,20 @@ func WithExtraAttributes(attrs templ.Attributes) Option {
 	}
 }
 
+// WithExtraClasses assigns additional classes to the Component.
+func WithExtraClasses(extraClasses ...classes.Class) Option {
+	return func(p *Props) {
+		for _, class := range extraClasses {
+			p.AddClass(class)
+		}
+	}
+}
+
 // NewMenu creates a new menu with the given options.
 func Build(options ...Option) *Props {
 	menu := &Props{
 		Attributes: attributes.New(),
+		Classes:    classes.New(),
 		Items:      items.New(),
 	}
 
@@ -111,19 +123,3 @@ func Build(options ...Option) *Props {
 
 	return menu
 }
-
-// type DropDownMenu struct {
-// 	Props
-// 	modifierBaseColor
-// }
-
-// // NewDropDownMenu creates a new menu wrapped in a dropdown with the given options.
-// func NewDropDownMenu(options ...Option[*DropDownMenu]) *DropDownMenu {
-// 	menu := &DropDownMenu{}
-
-// 	for _, option := range options {
-// 		menu = option(menu)
-// 	}
-
-// 	return menu
-// }
