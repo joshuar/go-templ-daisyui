2f596478c6e93fafaab66e42eb96491ffe5df2ab
Joshua Rich <joshua.rich@gmail.com>
2025-03-14 16:33:09 +1000

    feat(components/input): :sparkles: add fileinput component

---
+++ input/fileinput/fileinput.go
@@ -0,0 +1,87 @@
+// Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
+// SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+package fileinput
+
+import (
+	"github.com/a-h/templ"
+	components "github.com/joshuar/go-templ-daisyui"
+	"github.com/joshuar/go-templ-daisyui/attributes"
+	"github.com/joshuar/go-templ-daisyui/classes"
+	"github.com/joshuar/go-templ-daisyui/modifiers/color"
+	"github.com/joshuar/go-templ-daisyui/modifiers/size"
+)
+
+type Props struct {
+	*attributes.Attributes
+	*classes.Classes
+	themeColor color.ThemeColor
+	stateColor color.StateColor
+	size       size.ResponsiveSize
+}
+
+type Option components.Option[*Props]
+
+// WithSize option sets the responsive size of the component.
+func WithSize(inputSize size.ResponsiveSize) Option {
+	return func(input *Props) {
+		input.size = inputSize
+	}
+}
+
+// WithThemeColor option sets a theme color on the component.
+func WithThemeColor(themeColor color.ThemeColor) Option {
+	return func(input *Props) {
+		input.themeColor = themeColor
+	}
+}
+
+// WithStateColor option sets a state color on the component.
+func WithStateColor(stateColor color.StateColor) Option {
+	return func(input *Props) {
+		input.stateColor = stateColor
+	}
+}
+
+// WithName option sets a name attribute on the component.
+func WithName(name attributes.Name) Option {
+	return func(input *Props) {
+		input.SetName(name)
+	}
+}
+
+// WithID option sets an id attribute on the component.
+func WithID(id string) Option {
+	return func(input *Props) {
+		input.SetID(id)
+	}
+}
+
+// WithExtraAttributes option sets additional attributes on the component.
+func WithExtraAttributes(attrs templ.Attributes) Option {
+	return func(input *Props) {
+		input.AddAttributes(attrs)
+	}
+}
+
+// WithExtraClasses option assigns additional classes to the component.
+func WithExtraClasses(extraClasses ...classes.Class) Option {
+	return func(input *Props) {
+		for _, class := range extraClasses {
+			input.AddClass(class)
+		}
+	}
+}
+
+func Build(options ...Option) *Props {
+	props := &Props{
+		Attributes: attributes.New(),
+		Classes:    classes.New(),
+	}
+
+	for _, option := range options {
+		option(props)
+	}
+
+	return props
+}
