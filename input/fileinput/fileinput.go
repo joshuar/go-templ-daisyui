00f06d9e40fdf3552a18df8968848813c25d51fd
Joshua Rich <joshua.rich@gmail.com>
2025-03-18 09:30:07 +1000

    feat(components/input): :sparkles: add validation to fileinput

--- input/fileinput/fileinput.go
+++ input/fileinput/fileinput.go
@@ -8,6 +8,7 @@ import (
 	components "github.com/joshuar/go-templ-daisyui"
 	"github.com/joshuar/go-templ-daisyui/attributes"
 	"github.com/joshuar/go-templ-daisyui/classes"
+	"github.com/joshuar/go-templ-daisyui/input/validation"
 	"github.com/joshuar/go-templ-daisyui/modifiers/color"
 	"github.com/joshuar/go-templ-daisyui/modifiers/size"
 )
@@ -15,6 +16,7 @@ import (
 type Props struct {
 	*attributes.Attributes
 	*classes.Classes
+	*validation.Validation
 	themeColor color.ThemeColor
 	stateColor color.StateColor
 	size       size.ResponsiveSize
@@ -57,6 +59,27 @@ func WithID(id string) Option {
 	}
 }
 
+// Required marks the field as required, which will trigger and enforce HTML5
+// input validation.
+func Required() Option {
+	return func(p *Props) {
+		p.SetRequired(true)
+	}
+}
+
+// Validate option ensures that HTML5 validation will be applied to the input.
+func Validate() Option {
+	return func(p *Props) {
+		p.SetValidation(true)
+	}
+}
+
+func ValidateHint(hint string) Option {
+	return func(p *Props) {
+		p.SetHint(hint)
+	}
+}
+
 // WithExtraAttributes option sets additional attributes on the component.
 func WithExtraAttributes(attrs templ.Attributes) Option {
 	return func(input *Props) {
@@ -77,6 +100,7 @@ func Build(options ...Option) *Props {
 	props := &Props{
 		Attributes: attributes.New(),
 		Classes:    classes.New(),
+		Validation: validation.New(),
 	}
 
 	for _, option := range options {
