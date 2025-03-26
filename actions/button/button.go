0c8aee5c61ffca5ed33eae21ee23547cb12d27dc
Joshua Rich <joshua.rich@gmail.com>
2025-03-27 09:00:51 +1000

    refactor(components/button): :bulb: improve comments

--- actions/button/button.go
+++ actions/button/button.go
@@ -1,6 +1,9 @@
 // Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
 // SPDX-License-Identifier: 	AGPL-3.0-or-later
 
+// Package button represents a DaisyUI Button component.
+//
+// https://daisyui.com/components/button/
 package button
 
 import (
@@ -23,6 +26,7 @@ const (
 	Block
 )
 
+// Option is a functional option for a Button.
 type Option components.Option[*Props]
 
 // Shape sets the shape of the button.
@@ -33,6 +37,7 @@ type modifierShape struct {
 	shapeOutline bool
 }
 
+// Props represents the properties for a DaisyUI Button component.
 type Props struct {
 	*attributes.Attributes
 	size size.ResponsiveSize
@@ -48,87 +53,88 @@ type Props struct {
 // AsShape sets the shape of the button, such as square or circle. If this option
 // is not used, the button will be a regular button.
 func AsShape(shape Shape, outline bool) Option {
-	return func(p *Props) {
+	return func(btn *Props) {
 		if shape == 0 {
 			return
 		}
 
-		p.shape = shape
-		p.shapeOutline = outline
+		btn.shape = shape
+		btn.shapeOutline = outline
 	}
 }
 
 // WithContent sets the content for the Button.
 func WithContent(content any) Option {
-	return func(p *Props) {
-		p.content = components.SetContent(content)
+	return func(btn *Props) {
+		btn.content = components.SetContent(content)
 	}
 }
 
 // WithAutoFocus will ensure the Button is focused when displayed.
 func WithAutoFocus() Option {
-	return func(p *Props) {
-		p.autofocus = true
+	return func(btn *Props) {
+		btn.autofocus = true
 	}
 }
 
 // WithoutClickAnimation disables the click animation on the button.
 func WithoutClickAnimation() Option {
-	return func(p *Props) {
-		p.noClickAnimation = true
+	return func(btn *Props) {
+		btn.noClickAnimation = true
 	}
 }
 
 // WithText will set the text to display within the Badge.
 func WithSize(btnSize size.ResponsiveSize) Option {
-	return func(p *Props) {
-		p.size = btnSize
+	return func(btn *Props) {
+		btn.size = btnSize
 	}
 }
 
 func WithThemeColor(themeColor color.ThemeColor, outline bool) Option {
-	return func(p *Props) {
-		p.ThemeColorProps = color.NewThemeColor(themeColor, outline)
+	return func(btn *Props) {
+		btn.ThemeColorProps = color.NewThemeColor(themeColor, outline)
 	}
 }
 
 func WithStateColor(stateColor color.StateColor, outline bool) Option {
-	return func(p *Props) {
-		p.StateColorProps = color.NewStateColor(stateColor, outline)
+	return func(btn *Props) {
+		btn.StateColorProps = color.NewStateColor(stateColor, outline)
 	}
 }
 
 // WithStyle will apply the given style to the component.
 func WithStyle(value style.Style) Option {
-	return func(p *Props) {
-		p.style = value
+	return func(btn *Props) {
+		btn.style = value
 	}
 }
 
 func WithName(name attributes.Name) Option {
-	return func(p *Props) {
-		p.SetName(name)
+	return func(btn *Props) {
+		btn.SetName(name)
 	}
 }
 
 func WithID(id string) Option {
-	return func(p *Props) {
-		p.SetID(id)
+	return func(btn *Props) {
+		btn.SetID(id)
 	}
 }
 
 func WithValue(value attributes.Value) Option {
-	return func(p *Props) {
-		p.SetValue(value)
+	return func(btn *Props) {
+		btn.SetValue(value)
 	}
 }
 
 func WithExtraAttributes(attrs templ.Attributes) Option {
-	return func(p *Props) {
-		p.AddAttributes(attrs)
+	return func(btn *Props) {
+		btn.AddAttributes(attrs)
 	}
 }
 
+// Build generates Button properties from the given options.
 func Build(options ...Option) *Props {
 	btn := &Props{
 		Attributes:      attributes.New(),
