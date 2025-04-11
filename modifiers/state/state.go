045f32e1409fe3668f937d9908f7275aaa85c0d8
Joshua Rich <joshua.rich@gmail.com>
2025-04-11 14:48:16 +1000

    feat(modifiers/state): :sparkles: add a method to convert a string to a state value

--- modifiers/state/state.go
+++ modifiers/state/state.go
@@ -3,12 +3,14 @@
 
 package state
 
+//go:generate go tool golang.org/x/tools/cmd/stringer -type=State -linecomment -output state.gen.go
+
 const (
 	MinInvalid State = iota
-	Info
-	Success
-	Warning
-	Error
+	Info             // info
+	Success          // success
+	Warning          // warning
+	Error            // error
 	MaxInvalid
 )
 
@@ -18,4 +20,19 @@ type State int
 // Valid returns whether the state is a valid value.
 func (state State) Valid() bool {
 	return state > MinInvalid && state < MaxInvalid
+}
+
+// AsState attempts to match a string representing a state to the appropriate state value.
+func AsState[T ~string](value T) State {
+	switch string(value) {
+	case Info.String():
+		return Info
+	case Success.String():
+		return Success
+	case Warning.String():
+		return Warning
+	case Error.String():
+		return Error
+	}
+	return 0
 }
