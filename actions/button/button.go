898daea86d4bcceb30f6a27b79cd1dc31573d8dc
Joshua Rich <joshua.rich@gmail.com>
2025-07-14 15:53:59 +1000

    feat(components/button): :sparkles: modernize button component
    - move to implementing templ.Component interface for button
    - collapse class-based options to WithClasses option

--- actions/button/button.go
+++ actions/button/button.go
@@ -33,48 +33,6 @@ func (p *Props) SetValue(value attributes.Value) {
 	p.attributes.SetValue(value)
 }
 
-// WithContent sets the content for the Button.
-func WithContent(content any) Option {
-	return func(btn *Props) {
-		btn.content = components.SetContent(content)
-	}
-}
-
-// WithAutoFocus will ensure the Button is focused when displayed.
-func WithAutoFocus() Option {
-	return func(btn *Props) {
-		btn.autofocus = true
-	}
-}
-
-// WithoutClickAnimation disables the click animation on the button.
-func WithoutClickAnimation() Option {
-	return func(btn *Props) {
-		btn.animated = false
-	}
-}
-
-// WithText will set the text to display within the Badge.
-func WithSize(size Size) Option {
-	return func(btn *Props) {
-		btn.classes.Add(size)
-	}
-}
-
-func WithColor(color Color) Option {
-	return func(btn *Props) {
-		btn.classes.Add(color)
-	}
-}
-
-// AsShape sets the shape of the button, such as square or circle. If this option
-// is not used, the button will be a regular button.
-func AsShape(shape Shape) Option {
-	return func(btn *Props) {
-		btn.classes.Add(shape)
-	}
-}
-
 func AsSubmit() Option {
 	return func(p *Props) {
 		p.attributes.SetAttribute("type", "submit")
@@ -84,13 +42,6 @@ func AsSubmit() Option {
 func AsReset() Option {
 	return func(p *Props) {
 		p.attributes.SetAttribute("type", "reset")
-	}
-}
-
-// WithStyle will apply the given style to the component.
-func WithStyle(style Style) Option {
-	return func(btn *Props) {
-		btn.classes.Add(style)
 	}
 }
 
@@ -112,15 +63,15 @@ func WithValue(value attributes.Value) Option {
 	}
 }
 
-func WithExtraAttributes(attrs templ.Attributes) Option {
+func WithAttributes(attrs templ.Attributes) Option {
 	return func(btn *Props) {
 		btn.attributes.AddAttributes(attrs)
 	}
 }
 
-func WithExtraClasses(extraClasses ...components.Class) Option {
+func WithClasses(classes ...components.Class) Option {
 	return func(p *Props) {
-		p.classes.Add(extraClasses...)
+		p.classes.Add(classes...)
 	}
 }
 
