2f6fde14552b3184ddcf768200b48a4e2c64897b
Joshua Rich <joshua.rich@gmail.com>
2025-07-14 15:57:25 +1000

    feat(components/form): :sparkles: modernize fieldset and form components
    - implement templ.Component interface
    - collapse class options

--- input/fieldset/fieldset.go
+++ input/fieldset/fieldset.go
@@ -8,7 +8,6 @@ import (
 	components "github.com/joshuar/go-templ-daisyui"
 	"github.com/joshuar/go-templ-daisyui/attributes"
 	"github.com/joshuar/go-templ-daisyui/input"
-	"github.com/joshuar/go-templ-daisyui/modifiers/color"
 )
 
 // FieldProps represents an input field in a fieldset.
@@ -20,8 +19,6 @@ type FieldProps struct {
 // Props represents a fieldset.
 type Props struct {
 	legend     string
-	fields     []*FieldProps
-	baseColor  color.BaseColor
 	attributes *attributes.Attributes
 	classes    *components.Classes
 }
@@ -35,14 +32,6 @@ func WithLegend(legend string) Option {
 	}
 }
 
-// WithField option adds the given input with an optional label as a field in
-// the fieldset. Call this multiple times to add multiple inputs.
-func WithField(label string, field input.Input) Option {
-	return func(fieldset *Props) {
-		fieldset.fields = append(fieldset.fields, &FieldProps{label: label, input: field})
-	}
-}
-
 // WithID option sets the id attribute on the component.
 func WithID(id string) Option {
 	return func(fieldset *Props) {
@@ -51,23 +40,16 @@ func WithID(id string) Option {
 }
 
 // WithExtraAttributes option sets additional attributes on the component.
-func WithExtraAttributes(attrs templ.Attributes) Option {
+func WithAttributes(attrs templ.Attributes) Option {
 	return func(fieldset *Props) {
 		fieldset.attributes.AddAttributes(attrs)
 	}
 }
 
 // WithExtraClasses option assigns additional classes to the component.
-func WithExtraClasses(extraClasses ...components.Class) Option {
+func WithClasses(extraClasses ...components.Class) Option {
 	return func(fieldset *Props) {
 		fieldset.classes.Add(extraClasses...)
-	}
-}
-
-// WithBaseColor option sets a base color on the component.
-func WithBaseColor(baseColor color.BaseColor) Option {
-	return func(fieldset *Props) {
-		fieldset.baseColor = baseColor
 	}
 }
 
