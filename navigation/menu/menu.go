9999efb17174843d9148a4aaf228cbc7f87c6840
Joshua Rich <joshua.rich@gmail.com>
2025-07-18 09:44:37 +1000

    feat(components/menu): :sparkles: modernize menu component
    - implement templ.Component interface
    - collapse options

--- navigation/menu/menu.go
+++ navigation/menu/menu.go
@@ -10,80 +10,16 @@ import (
 	"github.com/a-h/templ"
 	components "github.com/joshuar/go-templ-daisyui"
 	"github.com/joshuar/go-templ-daisyui/attributes"
-	"github.com/joshuar/go-templ-daisyui/items"
-	"github.com/joshuar/go-templ-daisyui/modifiers/breakpoints"
-	"github.com/joshuar/go-templ-daisyui/modifiers/color"
-	"github.com/joshuar/go-templ-daisyui/modifiers/size"
 )
 
 // Props represents the properties for a DaisyUI menu component.
 type Props struct {
 	attributes *attributes.Attributes
 	classes    *components.Classes
-	title      templ.Component
-	size       Size
-	baseColor  color.BaseColor
-	breakpoints.Breakpoints
-	*items.Items
-	itemClasses *components.Classes
 }
 
 // Option is a functional option to apply menu properties.
 type Option components.Option[*Props]
-
-// WithMenuTitle sets the menu title.
-func WithMenuTitle(title any) Option {
-	return func(p *Props) {
-		p.title = components.SetContent(title)
-	}
-}
-
-// WithLayout sets the layout of the menu, either vertically or horizontally. If
-// this option is not specified, the menu will default to a vertical layout.
-func WithLayout(layout Layout) Option {
-	return func(p *Props) {
-		p.classes.Add(layout)
-	}
-}
-
-func WithHiddenBreakpoint(from size.ResponsiveSize) Option {
-	return func(p *Props) {
-		p.SetHiddenBreakpoint(from)
-	}
-}
-
-func WithRevealedBreakpoint(from size.ResponsiveSize) Option {
-	return func(p *Props) {
-		p.SetRevealedBreakpoint(from)
-	}
-}
-
-func WithSize(menuSize Size) Option {
-	return func(p *Props) {
-		p.size = menuSize
-	}
-}
-
-func WithBaseColor(base color.BaseColor) Option {
-	return func(p *Props) {
-		p.baseColor = base
-	}
-}
-
-// WithItems option sets the list of items to display in the dropdown menu. Each
-// item will be wrapped in an list element.
-func WithItems(listItems ...templ.Component) Option {
-	return func(p *Props) {
-		p.ReplaceItems(listItems...)
-	}
-}
-
-// WithItemExtraClasses assigns additional classes to each item of the menu.
-func WithExtraItemClasses(extraClasses ...components.Class) Option {
-	return func(p *Props) {
-		p.Items.SetClasses(extraClasses...)
-	}
-}
 
 func WithID(id string) Option {
 	return func(p *Props) {
@@ -91,14 +27,14 @@ func WithID(id string) Option {
 	}
 }
 
-func WithExtraAttributes(attrs templ.Attributes) Option {
+func WithAttributes(attrs templ.Attributes) Option {
 	return func(p *Props) {
 		p.attributes.AddAttributes(attrs)
 	}
 }
 
 // WithExtraClasses assigns additional classes to the Component.
-func WithExtraClasses(extraClasses ...components.Class) Option {
+func WithClasses(extraClasses ...components.Class) Option {
 	return func(p *Props) {
 		p.classes.Add(extraClasses...)
 	}
@@ -107,10 +43,8 @@ func WithExtraClasses(extraClasses ...components.Class) Option {
 // Build generates menu properties with the given options.
 func Build(options ...Option) *Props {
 	menu := &Props{
-		Items:       items.New(),
-		attributes:  attributes.New(),
-		classes:     components.NewClasses(),
-		itemClasses: components.NewClasses(),
+		attributes: attributes.New(),
+		classes:    components.NewClasses(),
 	}
 
 	for _, option := range options {
